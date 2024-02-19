131b01e0069e76a7d5318237e8cfc9d616c73189
Talha shiekh <tk.shiekh4567@gmail.com>
2024-02-19 03:27:53 -0800

    Pending Learning of middlewears

--- express.js
+++ express.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 
-const COURSES = [
+let COURSES = [
     {id:1,name:"Javascript"},
     {id:2,name:"Python"},
     {id:3,name:"Java"},
@@ -16,6 +16,35 @@ const COURSES = [
 app.get("/",(req,res) => {
    res.send("Learning Node js from Scaler Acadmey");
 });
+
+app.put("/courses/:course",(req,res) => {
+    let course = COURSES.find(c => c.name === req.params.course);
+    if(!course) res.status(404).send("The Course you are looking for does not exists")
+
+    course.name = req.body.name;
+    res.send(`Welcome to the ${course.name} Course`)
+})
+
+
+//  app.delete("/courses/:course",(req,res) => {
+//     let updatedCourses = COURSES.filter(c => c.name !== req.params.course);
+
+//     COURSES = updatedCourses;
+
+//     res.send(COURSES);
+//  })
+
+app.delete("/courses/:id",(req,res) => {
+    let course = COURSES.find(c => c.id === Number(req.params.id));
+
+    if(!course) res.status(404).send("The Course you are looking for does not exists")
+
+    let index = COURSES.indexOf(course);
+
+    COURSES.splice(index,1);
+
+    res.send(course);
+})
 
 app.get("/courses",(req,res) => {
     res.send(COURSES)
