e7be3804a9768c5aa26121674923576c546363d5
Talha shiekh <tk.shiekh4567@gmail.com>
2024-03-14 03:41:00 -0700

    Pending Data Validation in mongo db

--- MONGO DB/index.js
+++ MONGO DB/index.js
@@ -9,7 +9,8 @@ const CourseSchema = new  mongoose.Schema({
     creator:String,
     description:String,
     date:{type:Date,default:Date.now()},
-    isDone:Boolean
+    isDone:Boolean,
+    rating:Number
 })
 
 let FirstCourse =  mongoose.model("FirstCourse",CourseSchema);
@@ -18,9 +19,38 @@ const course = new FirstCourse({
     name:"Python",
     creator:"Talha shiekh",
     description:"Ultimate python course including all the concepts of javascript from zero to advanced",
-    isDone:false
+    isDone:false,
+    rating:Math.floor(Math.random() * 2)
 })
 
-let resultedCourse = await course.save();
+// await course.save();/
 
-console.log(resultedCourse)
+// console.log(resultedCourse);
+
+// console.log("Talha shiekh")
+
+const findedCourse = await FirstCourse.find({rating:{$in:[0,1]}}).and([{name:"Python"},{rating:1}])
+
+console.log(findedCourse);
+
+async function UpdateCourse(id){
+    let course =  await FirstCourse.findById(id) ;
+    if(!course) return;
+
+    course.name = "JavaScript";
+    course.creator = "Harry bhai";
+
+    const updatedCourse = await course.save()
+
+    console.log(updatedCourse)
+}
+
+// UpdateCourse("65f2cc34294e1c9be09d1ee7")
+
+async function DeleteCourse(id){
+    let DeletedCourse = await FirstCourse.findByIdAndDelete(id);
+
+    return DeletedCourse;
+}
+
+DeleteCourse("65f2cc34294e1c9be09d1ee7")
